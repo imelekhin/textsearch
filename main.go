66ea1e3949d807a1e432bd07743907be91d966f4
Ivan <imelekhin@gmail.com>
2020-02-09 22:08:56 +0300

    ver 0.8

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"encoding/json"
 	"errors"
+	"flag"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"regexp"
 	"strings"
+	"syscall"
+	"time"
+
+	"github.com/segmentio/kafka-go"
 )
 
 type fieldsHashTable map[string]*searchList
@@ -17,9 +26,66 @@ type searchList struct {
 	comments   []string
 }
 
+var kafkaURL = flag.String("kafka-broker", "127.0.0.1:9092", "Kafka broker URL list")
+var intopic = flag.String("kafka-in-topic", "notopic", "Kafka topic to read from")
+var outtopic = flag.String("kafka-out-topic", "notopic", "Kafka topic to write to")
+var groupID = flag.String("kafka-group", "nogroup", "Kafka group")
+var metricsport = flag.String("metric-port", "1234", "Port to expose metrics")
+
+var filename = flag.String("config", "textsearch.cfg", "config file path name")
+
 var fields fieldsHashTable
 
 var logger *log.Logger
+
+var reader *kafka.Reader
+var writer *kafka.Writer
+
+type stat_reader struct {
+}
+
+type stat_writer struct {
+}
+
+func (s *stat_reader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	str, _ := json.Marshal(reader.Stats())
+	w.Write([]byte(str))
+}
+
+func (s *stat_writer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	str, _ := json.Marshal(writer.Stats())
+	w.Write([]byte(str))
+}
+
+func getKafkaReader(kafkaURL, topic, groupID string, logger *log.Logger) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:        brokers,
+		GroupID:        groupID,
+		Topic:          topic,
+		MinBytes:       10e3, // 10KB
+		MaxBytes:       10e6, // 10MB
+		StartOffset:    kafka.LastOffset,
+		CommitInterval: 1 * time.Second,
+		QueueCapacity:  10000,
+		//ReadBackoffMin: 1 * time.Millisecond,
+		ErrorLogger: logger,
+	})
+}
+
+func newKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+	return kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{kafkaURL},
+		Topic:   topic,
+		//Balancer: &kafka.LeastBytes{},
+		ErrorLogger: logger,
+		Async:       true,
+	})
+}
 
 func splitLine(line string, linenum int) ([]string, error) {
 	res := []string{"", ""}
@@ -49,9 +115,42 @@ func splitLine(line string, linenum int) ([]string, error) {
 	return res, nil
 }
 
-func loadSearches(filename string) []regexp.Regexp {
+func loadRegexpFromFile(filename string) ([]regexp.Regexp, error) {
 
-	res := make([]regexp.Regexp, 0)
+	var res []regexp.Regexp
+
+	file, err := os.Open(filename)
+
+	defer file.Close()
+
+	if err != nil {
+		logger.Print("Error opening file ", filename)
+		return nil, errors.New("")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+		logger.Print("Error opening file ", filename)
+		return nil, errors.New("")
+	}
+
+	for scanner.Scan() {
+		str := strings.TrimSpace(scanner.Text())
+		r, err := regexp.Compile(str)
+		if err != nil {
+			logger.Print("Cannot compile rgexp ", str, " file ", filename)
+			return nil, errors.New("")
+		}
+
+		res = append(res, *r)
+
+	}
+
+	return res, nil
+}
+
+func loadSearches(filename string) {
 
 	file, err := os.Open(filename)
 
@@ -112,6 +211,26 @@ func loadSearches(filename string) []regexp.Regexp {
 
 				} else {
 					//logger.Printf("Line %d: this is file", line)
+					res, err := splitLine(currline, line)
+					if err != nil {
+						line++
+						break
+					}
+
+					fields[currentfield].comments = append(fields[currentfield].comments, res[1])
+
+					rgp, err := loadRegexpFromFile(res[0])
+
+					if err != nil {
+						line++
+						break
+					}
+
+					fields[currentfield].regexps = rgp
+
+					for i := 0; i < len(fields[currentfield].regexps); i++ {
+						fields[currentfield].commentIdx = append(fields[currentfield].commentIdx, len(fields[currentfield].comments)-1)
+					}
 
 				}
 				line++
@@ -145,26 +264,110 @@ func loadSearches(filename string) []regexp.Regexp {
 
 	}
 
-	return res
+}
+
+func sendAlarm(message map[string]interface{}, regexp string, comment string) {
+	logger.Print("In message: ", message["message"], " found regexp :", regexp, " ", comment)
 }
 
 func init() {
-	logger = log.New(os.Stdout, "ipsearch: ", log.Ldate|log.Ltime|log.Lshortfile)
-	//comments = make([]string, 1)
-	//comments[0] = "Bad regexp found"
+	logger = log.New(os.Stdout, "textsearch: ", log.Ldate|log.Ltime)
 	fields = make(fieldsHashTable, 0)
+	flag.Parse()
 
-}
-
-func main() {
-	loadSearches("strings.txt")
+	logger.Print("Loading config from ", *filename)
+	loadSearches(*filename)
 
 	for k, j := range fields {
 		logger.Print("Field is: ", k)
 		for i := 0; i < (len(j.regexps)); i++ {
-			logger.Print("On regexp '", j.regexps[i].String(), "' comment is ", j.comments[i], " line ", i)
+			logger.Print("On regexp '", j.regexps[i].String(), "' comment is ", j.comments[j.commentIdx[i]], " line ", i)
 		}
 
 	}
+	logger.Print("Done loading config")
 
+}
+
+func main() {
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	reader = getKafkaReader(*kafkaURL, *intopic, *groupID, logger)
+	writer = newKafkaWriter(*kafkaURL, *outtopic)
+
+	quit := make(chan struct{})
+
+	defer func() {
+		reader.Close()
+		writer.Close()
+		close(quit)
+	}()
+
+	go func() {
+		r := &stat_reader{}
+		w := &stat_writer{}
+		http.Handle("/metrics/reader", r)
+		http.Handle("/metrics/writer", w)
+		logger.Fatal(http.ListenAndServe(":"+*metricsport, nil))
+	}()
+
+	logger.Print("start consuming ... !!")
+
+	start := time.Now()
+	msgcount := 0
+
+	//  var readTime time.Duration = 0
+	//	var searchTime time.Duration = 0
+	//  var writeTime time.Duration = 0
+loop:
+	for {
+
+		select {
+		case sig := <-sigs:
+			logger.Print(sig)
+			break loop
+		default:
+			//startTime := time.Now()
+
+			m, err := reader.ReadMessage(context.Background())
+			//readTime = readTime + time.Since(startTime)
+
+			msgcount++
+
+			if err != nil {
+				logger.Print(err)
+				break
+			}
+
+			var f interface{}
+
+			err = json.Unmarshal([]byte(m.Value), &f)
+
+			if err != nil {
+				logger.Print(err)
+				break
+			}
+
+			msg := f.(map[string]interface{})
+
+			for k, j := range msg {
+				value, found := fields[k]
+				if found {
+					for i := 0; i < len((*value).regexps); i++ {
+						if (*value).regexps[i].MatchString(j.(string)) {
+							sendAlarm(msg, (*value).regexps[i].String(), (*value).comments[(*value).commentIdx[i]])
+						}
+					}
+				}
+			}
+		}
+	}
+
+	logger.Print("Terminating")
+	elapsed := time.Since(start)
+	logger.Printf("Message processed %d in %s", msgcount, elapsed)
+	//logger.Printf("Read time: %s Search time: %s Write time %s", readTime, searchTime, writeTime)
 }
